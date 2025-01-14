accac1756a86dd1cb6bd26fecf0c8de6df68e351
Bhavyam Mittal <bhavyamm01@gmail.com>
2025-01-14 23:46:27 +0530

    added vercel domain

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['localhost']
+    domains: ['localhost', "https://property-listing-platform-p9k2-f7cd97glu-bhavyamms-projects.vercel.app"]
   },
 };
 
