e855abf0c5cae08403527d671daeb65b00306d9a
Bhavyam Mittal <bhavyamm01@gmail.com>
2025-01-15 00:01:33 +0530

    fixing cors issue

--- app/api/properties/[id]/route.ts
+++ app/api/properties/[id]/route.ts
@@ -2,18 +2,18 @@ import { NextResponse, NextRequest } from "next/server";
 import { properties } from "../../dummyData";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+
 export async function GET(
   request: NextRequest,
   { params }: any
 ): Promise<NextResponse> {
   const { id } = params as { id: string };
   const property = properties.find((prop) => prop.id === id);
-
-  const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  };
 
   if (!property) {
     return NextResponse.json(
@@ -26,4 +26,11 @@ export async function GET(
   }
 
   return NextResponse.json(property, { headers: corsHeaders });
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
 }
--- app/api/properties/route.ts
+++ app/api/properties/route.ts
@@ -1,12 +1,19 @@
 import { NextResponse } from "next/server";
 import { properties } from "../dummyData";
 
-export async function GET() {
-  const corsHeaders = {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
-  };
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
 
+export async function GET() {
   return NextResponse.json(properties, { headers: corsHeaders });
+}
+
+export async function OPTIONS() {
+  return new NextResponse(null, {
+    status: 204,
+    headers: corsHeaders,
+  });
 }
